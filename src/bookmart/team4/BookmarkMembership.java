cff4c3be25dc7ea3bc2311672db057b26363c724
raycantu <ray.cantu.16@gmail.com>
2016-03-11 12:44:47 -0600

    Update BookmarkMembership.java

--- src/bookmart/team4/BookmarkMembership.java
+++ src/bookmart/team4/BookmarkMembership.java
@@ -14,3 +14,40 @@ public class BookmarkMembership {
             
     private String anus;
 }
+
+public Abstract class BookmarkMembership
+{
+
+  String firstName;  // Holds first name of user
+  String lastName;  // Holds last name of user
+  String middleInitial;  // Holds middle initial
+  String password;  // Holds password string
+  String email;  // Holds email string
+  
+  abstract void setFirstName(String fName)
+  {
+      // Will later be used by children classes to set first name.
+  }
+  
+  abstract void setLastName(String lName)
+  {
+      // Will laster be used by children classes to set last name.
+  }
+  
+  abstract void setMiddleInitial(String mName)
+  {
+      // Will later be used by children classes to set middle initial.
+  }
+  
+  abstract void setPass(String pass)
+  {
+      // Will later be used by children classes to set password.
+  }
+  
+  abstract void setEmail(String mail)
+  {
+      // Will later be used by children classes to set email.
+  }
+  
+  
+}
